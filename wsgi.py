c2cd5d66051d4f67dc1f9ce04b7cab3720741d05
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-05-31 15:44:03 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Ahoj Světe!"
+    return "Ahoj Světe! Jak to de?"
 
 if __name__ == "__main__":
     application.run()
